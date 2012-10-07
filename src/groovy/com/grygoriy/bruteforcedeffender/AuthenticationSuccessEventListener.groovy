7a4626e2b5ed6c97542043e715f388bf9295f174
Burt Beckwith <beckwithb@vmware.com>
2012-10-06 21:49:45 -0400

    cleanup

--- src/groovy/com/grygoriy/bruteforcedeffender/AuthenticationSuccessEventListener.groovy
+++ src/groovy/com/grygoriy/bruteforcedeffender/AuthenticationSuccessEventListener.groovy
@@ -1,18 +1,15 @@
 package com.grygoriy.bruteforcedeffender
 
-import org.springframework.security.authentication.event.AuthenticationSuccessEvent
 import org.springframework.context.ApplicationListener
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent
 
 /**
- * User: Grygoriy Mykhalyunyo
- * Date: 7/14/12
- * Time: 6:16 PM
+ * @author Grygoriy Mykhalyunyo
  */
-class AuthenticationSuccessEventListener implements ApplicationListener<AuthenticationSuccessEvent>{
+class AuthenticationSuccessEventListener implements ApplicationListener<AuthenticationSuccessEvent> {
 
     LoginAttemptCacheService loginAttemptCacheService
 
-    @Override
     void onApplicationEvent(AuthenticationSuccessEvent e) {
         loginAttemptCacheService.loginSuccess(e.authentication.name)
     }
