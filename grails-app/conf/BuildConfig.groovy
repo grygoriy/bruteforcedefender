3132771f747b368e11c422f171146b7e40feae29
Burt Beckwith <beckwithb@vmware.com>
2012-10-06 21:45:47 -0400

    update to release plugin 2.0.4, removed unused tomcat plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -31,8 +31,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build(":tomcat:$grailsVersion",
-              ":release:1.0.0") {
+        build(':release:2.0.4', ':rest-client-builder:1.0.2') {
             export = false
         }
         compile ":spring-security-core:1.2.7.3"
